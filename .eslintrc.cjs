e923c0e26425ae7e54e971dfa320b04a23562601
AaronAngeloRagudos <aaronangeloworkemail@gmail.com>
2023-09-27 11:11:43 +0800

    follow-up config update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,13 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: true,
+    project: [
+      "./tsconfig.eslint.json",
+      "./packages/*/tsconfig.json"
+    ]
+  },
   extends: [
     "@bionic-reader/eslint-config",
     "plugin:prettier/recommended"
